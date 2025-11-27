668b6a6031f6a9a3f0c2f25785e940094894713d
leotrejodev <213811760+leotrejodev@users.noreply.github.com>
2025-11-27 12:00:57 -0300

    agregado productos.json y llamada fetch en index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { productos } from "./productos.js";
+// import { productos } from "./productos.js";
 import { agregarAlCarrito } from "./funcionesCarrito.js";
 import { obtenerCarrito } from "./storage.js";
 import { actualizarContador } from "./ui.js";
@@ -9,33 +9,45 @@ document.addEventListener("DOMContentLoaded", () => {
   const carrito = obtenerCarrito();
   actualizarContador(carrito);
 
-  productos.forEach((producto) => {
-    const tarjeta = document.createElement("article");
-    tarjeta.classList.add("card");
+  fetch("./data/productos.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error HTTP = ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((producto) => {
+        const tarjeta = document.createElement("article");
+        tarjeta.classList.add("card");
 
-    const img = document.createElement("img");
-    img.src = producto.img;
-    img.alt = producto.nombre;
+        const img = document.createElement("img");
+        img.src = producto.img;
+        img.alt = producto.nombre;
 
-    const titulo = document.createElement("h3");
-    titulo.textContent = producto.nombre;
+        const titulo = document.createElement("h3");
+        titulo.textContent = producto.nombre;
 
-    const precio = document.createElement("p");
-    precio.textContent = `$${producto.precio}`;
+        const precio = document.createElement("p");
+        precio.textContent = `$${producto.precio}`;
 
-    const boton = document.createElement("button");
-    boton.classList.add("btn");
-    boton.textContent = "Agregar al Carrito";
+        const boton = document.createElement("button");
+        boton.classList.add("btn");
+        boton.textContent = "Agregar al Carrito";
 
-    boton.addEventListener("click", () => {
-      agregarAlCarrito(producto);
+        boton.addEventListener("click", () => {
+          agregarAlCarrito(producto);
+        });
+
+        tarjeta.appendChild(img);
+        tarjeta.appendChild(titulo);
+        tarjeta.appendChild(precio);
+        tarjeta.appendChild(boton);
+
+        contenedor.appendChild(tarjeta);
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
     });
-
-    tarjeta.appendChild(img);
-    tarjeta.appendChild(titulo);
-    tarjeta.appendChild(precio);
-    tarjeta.appendChild(boton);
-
-    contenedor.appendChild(tarjeta);
-  });
 });
